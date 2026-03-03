420e3d35e0a198d9d259d63e6f2ed9e8aaa2549f
Violet <3551664030@qq.com>
2026-03-03 22:08:40 +0800

    Initial

---
+++ src/backend/src/main/java/com/fire/firepalaceorderingsys/FirePalaceOrderingSysApplication.java
@@ -0,0 +1,13 @@
+package com.fire.firepalaceorderingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirePalaceOrderingSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirePalaceOrderingSysApplication.class, args);
+	}
+
+}
