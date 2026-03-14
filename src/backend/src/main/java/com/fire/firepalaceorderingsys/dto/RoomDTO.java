c5940c9f615e97f1413cfaa9eb06112b1af2e704
Violet <3551664030@qq.com>
2026-03-14 17:05:00 +0800

    包厢模块

--- src/backend/src/main/java/com/fire/firepalaceorderingsys/dto/RoomDTO.java
+++ src/backend/src/main/java/com/fire/firepalaceorderingsys/dto/RoomDTO.java
@@ -1,5 +1,7 @@
 package com.fire.firepalaceorderingsys.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,21 +24,25 @@ public class RoomDTO {
     /**
      * 房间名称
      */
+    @NotBlank(message = "包厢名不能为空")
     private String roomName;
 
     /**
      * 容纳人数
      */
+    @NotNull(message = "容纳人数不能为空")
     private Integer capacity;
 
     /**
      * 最低消费
      */
+    @NotNull(message = "最低消费不能为空")
     private BigDecimal minConsume;
 
     /**
      * 状态: 0空闲 1使用中
      */
+    @NotNull(message = "包厢状态不能为空")
     private Integer status;
 
     /**
