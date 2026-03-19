837d0dad4999d7c768f150b07ec5653677cdad0f
hamburger <2251492297@qq.com>
2026-03-19 15:42:15 +0800

    feat: 完成category分类增删改查接口，更新API文档

--- src/backend/src/main/java/com/fire/firepalaceorderingsys/controller/CategoryController.java
+++ src/backend/src/main/java/com/fire/firepalaceorderingsys/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.fire.firepalaceorderingsys.controller;
 
+import com.fire.firepalaceorderingsys.common.Result;
 import com.fire.firepalaceorderingsys.pojo.Category;
 import com.fire.firepalaceorderingsys.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,31 +14,61 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * 新增分类接口
+     */
     @PostMapping("/add")
-    public String addCategory(@RequestBody Category category) {
+    public Result addCategory(@RequestBody Category category) {
         boolean isSuccess = categoryService.addCategory(category);
-        return isSuccess ? "新增分类成功" : "新增分类失败";
+        if (isSuccess) {
+            return Result.success("新增分类成功");
+        } else {
+            return Result.error("新增分类失败");
+        }
     }
 
+    /**
+     * 修改分类接口
+     */
     @PutMapping("/update")
-    public String updateCategory(@RequestBody Category category) {
+    public Result updateCategory(@RequestBody Category category) {
         boolean isSuccess = categoryService.updateCategory(category);
-        return isSuccess ? "修改分类成功" : "修改分类失败";
+        if (isSuccess) {
+            return Result.success("修改分类成功");
+        } else {
+            return Result.error("修改分类失败");
+        }
     }
 
+    /**
+     * 根据id删除分类接口
+     */
     @DeleteMapping("/delete/{id}")
-    public String deleteCategory(@PathVariable Long id) {
+    public Result deleteCategory(@PathVariable Long id) {
         boolean isSuccess = categoryService.deleteCategoryById(id);
-        return isSuccess ? "删除分类成功" : "删除分类失败";
+        if (isSuccess) {
+            return Result.success("删除分类成功");
+        } else {
+            return Result.error("删除分类失败");
+        }
     }
 
+    /**
+     * 根据id查询分类详情接口
+     */
     @GetMapping("/get/{id}")
-    public Category getCategoryById(@PathVariable Long id) {
-        return categoryService.getCategoryById(id);
+    public Result getCategoryById(@PathVariable Long id) {
+        Category category = categoryService.getCategoryById(id);
+        return Result.success(category);
     }
 
+    /**
+     * 查询全部分类列表接口
+     */
     @GetMapping("/list")
-    public List<Category> getAllCategoryList() {
-        return categoryService.getAllCategoryList();
+    public Result getAllCategoryList() {
+        List<Category> categoryList = categoryService.getAllCategoryList();
+        return Result.success(categoryList);
     }
+
 }
